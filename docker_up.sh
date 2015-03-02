ab5fc3bc495953d0628a316afa71f41bbf273d5f
a504082002 <a504082002@gmail.com>
2015-03-02 17:36:10 +0800

    add shiny server

--- docker_up.sh
+++ docker_up.sh
@@ -40,6 +40,29 @@ function ipythonUp(){
     echo "./docker_stop.sh $dockername"
 }
 
+function shinyUp(){
+    # if [ -z "$1" ]; then echo "var is unset"; fi
+    dockername=${USER}_shiny
+    declare -i shinyport="3838"
+
+    docker run -d -P \
+      --name $dockername \
+      -v $1:/srv/shiny-server/ \  # $1 is where you place shinyapps
+      -v /srv/shinylog/:/var/log/ \
+      -v $HOME:$HOME \
+      rocker/shiny
+
+    ipWithPort=$(docker port $dockername $shinyport)
+    declare -i openport=${ipWithPort#0.0.0.0:}
+
+    echo "CONTAINER_NAME: $dockername"
+    echo "Open 120.126.36.164:$openport in your browser."
+    echo "$HOME is mounted on $HOME."
+    echo "Write logs in /srv/shinylog/"
+    echo "To stop server, please execute:"
+    echo "./docker_stop.sh $dockername"
+}
+
 
 ## main ##
 case $1 in
@@ -48,5 +71,8 @@ case $1 in
     ;;
     ipython)
         ipythonUp
+    ;;
+    shiny)
+        shinyUp $2
     ;;
 esac
